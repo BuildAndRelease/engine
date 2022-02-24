7a2f37cb030275e5332289995d5fb04fc7bc02c3
yzh <yzhtracy@163.com>
2022-02-24 21:03:27 +0800

    fix:修复mac中文输入法回车键的问题

--- shell/platform/darwin/macos/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterViewController.mm
@@ -98,7 +98,7 @@ struct MouseState {
 /**
  * Private interface declaration for FlutterViewController.
  */
-@interface FlutterViewController () <FlutterViewReshapeListener>
+@interface FlutterViewController () <FlutterViewReshapeListener,FlutterChannelKeyResponderDelegate>
 
 /**
  * The tracking area used to generate hover events, if enabled.
@@ -452,14 +452,17 @@ static void CommonInit(FlutterViewController* controller) {
                                                                    callback:callback
                                                                    userData:userData];
                                             }]];
+    FlutterChannelKeyResponder *keyResponder = [[FlutterChannelKeyResponder alloc]
+                                                initWithChannel:[FlutterBasicMessageChannel
+                                                                    messageChannelWithName:@"flutter/keyevent"
+                                                                           binaryMessenger:_engine.binaryMessenger
+                                                                                     codec:[FlutterJSONMessageCodec
+                                                                                            sharedInstance]]];
   [_keyboardManager
-      addPrimaryResponder:[[FlutterChannelKeyResponder alloc]
-                              initWithChannel:[FlutterBasicMessageChannel
-                                                  messageChannelWithName:@"flutter/keyevent"
-                                                         binaryMessenger:_engine.binaryMessenger
-                                                                   codec:[FlutterJSONMessageCodec
-                                                                             sharedInstance]]]];
+      addPrimaryResponder:keyResponder];
+
   [_keyboardManager addSecondaryResponder:_textInputPlugin];
+    keyResponder.delegate = self;
 }
 
 - (void)addInternalPlugins {
@@ -772,4 +775,11 @@ static void CommonInit(FlutterViewController* controller) {
   [self dispatchMouseEvent:event phase:kHover];
 }
 
+#pragma mark -FlutterChannelKeyResponderDelegate
+
+- (BOOL)hasMarkedText {
+    return [_textInputPlugin hasMarkedText];
+}
+
 @end
+
