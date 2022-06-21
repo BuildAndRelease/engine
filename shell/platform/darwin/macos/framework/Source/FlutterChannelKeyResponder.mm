3a636642532436bbb092652561cf0978d047004d
yzh <yzhtracy@163.com>
2022-06-21 11:36:01 +0800

    fix: 调整代码格式

--- shell/platform/darwin/macos/framework/Source/FlutterChannelKeyResponder.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterChannelKeyResponder.mm
@@ -72,7 +72,7 @@
   } mutableCopy];
   //如果是回车键，先让看一下是否是预输入状态，如果是就直接跳过flutter层处理
   if (([keyMessage[@"keyCode"] intValue] == 36 || [keyMessage[@"keyCode"] intValue] == 76) &&
-    self.delegate != nil && [self.delegate hasMarkedText]) {
+      self.delegate != nil && [self.delegate hasMarkedText]) {
     callback(false);
     return;
   }
