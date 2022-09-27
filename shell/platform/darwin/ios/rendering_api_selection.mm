bb5d2fe81605d27e0ae827cbf0ce6659c24dba4f
johnson.zhong <514961926@qq.com>
2022-09-27 12:53:05 +0800

    refactor: 格式化代码

--- shell/platform/darwin/ios/rendering_api_selection.mm
+++ shell/platform/darwin/ios/rendering_api_selection.mm
@@ -16,20 +16,19 @@
 
 namespace flutter {
 #if SHELL_ENABLE_METAL
-
-struct utsname systemInfo;
+const NSArray* outDeviceArray = @[
+  @"iPhone1,1",  @"iPhone1,2",  @"iPhone2,1",  @"iPhone3,1",  @"iPhone3,2",  @"iPhone3,3",
+  @"iPhone4,1",  @"iPhone5,1",  @"iPhone5,2",  @"iPhone5,3",  @"iPhone5,4",  @"iPhone6,2",
+  @"iPhone7,2",  @"iPhone7,1",  @"iPhone8,1",  @"iPhone8,2",  @"iPhone8,3",  @"iPhone8,4",
+  @"iPhone9,1",  @"iPhone9,3",  @"iPhone9,2",  @"iPhone9,4",  @"iPhone10,1", @"iPhone10,4",
+  @"iPhone10,2", @"iPhone10,5", @"iPhone10,3", @"iPhone10,6", @"iPhone11,8"
+];
 
 bool ShouldUseMetalRenderer() {
+  struct utsname systemInfo;
   uname(&systemInfo);
   NSString* platform = [NSString stringWithCString:systemInfo.machine
                                           encoding:NSASCIIStringEncoding];
-  NSArray* outDeviceArray = @[
-    @"iPhone1,1",  @"iPhone1,2",  @"iPhone2,1",  @"iPhone3,1",  @"iPhone3,2",  @"iPhone3,3",
-    @"iPhone4,1",  @"iPhone5,1",  @"iPhone5,2",  @"iPhone5,3",  @"iPhone5,4",  @"iPhone6,2",
-    @"iPhone7,2",  @"iPhone7,1",  @"iPhone8,1",  @"iPhone8,2",  @"iPhone8,3",  @"iPhone8,4",
-    @"iPhone9,1",  @"iPhone9,3",  @"iPhone9,2",  @"iPhone9,4",  @"iPhone10,1", @"iPhone10,4",
-    @"iPhone10,2", @"iPhone10,5", @"iPhone10,3", @"iPhone10,6", @"iPhone11,8"
-  ];
   if (![outDeviceArray containsObject:platform]) {
     return false;
   } else {
